c6a7885c1709965d605ffbf47d055b0676fb74e4
kabinny <kabinny@gmail.com>
2024-01-07 16:49:41 +0900

    Firebase Auth 세팅

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,15 @@
 import { Layout } from 'components/Layout'
 import Router from 'components/Router'
+import { app } from 'firebaseApp'
+import { getAuth } from 'firebase/auth'
+import { useState } from 'react'
 
 function App() {
+  const auth = getAuth(app)
+  const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
+    !!auth?.currentUser
+  )
+
   return (
     <Layout>
       <Router />
