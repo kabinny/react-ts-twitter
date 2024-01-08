52272c7fe4ec0c5a0f3c400dbb4e8d2569f4bec4
kabinny <kabinny@gmail.com>
2024-01-08 21:46:01 +0900

    이메일과 패스워드로 회원가입

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import { Layout } from 'components/Layout'
 import Router from 'components/Router'
 import { app } from 'firebaseApp'
-import { getAuth } from 'firebase/auth'
-import { useState } from 'react'
+import { getAuth, onAuthStateChanged } from 'firebase/auth'
+import { useEffect, useState } from 'react'
+import { ToastContainer } from 'react-toastify'
+import 'react-toastify/dist/ReactToastify.css'
 
 function App() {
   const auth = getAuth(app)
+  const [init, setInit] = useState<boolean>(false)
   const [isAuthenticated, setIsAuthenticated] = useState<boolean>(
     !!auth?.currentUser
   )
 
+  // firebase auth 상태 실시간 감지
+  useEffect(() => {
+    onAuthStateChanged(auth, (user) => {
+      if (user) {
+        setIsAuthenticated(true)
+      } else {
+        setIsAuthenticated(false)
+      }
+      setInit(true)
+    })
+  }, [auth])
+
   return (
     <Layout>
-      <Router />
+      <ToastContainer />
+      {init ? <Router isAuthenticated={isAuthenticated} /> : <>loading</>}
     </Layout>
   )
 }
