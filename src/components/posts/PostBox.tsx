6dc968d7ae73f749c90777339bbd2277f8a75f3a
kabinny <kabinny@gmail.com>
2024-02-02 21:37:23 +0900

    이미지 삭제

--- src/components/posts/PostBox.tsx
+++ src/components/posts/PostBox.tsx
@@ -1,6 +1,7 @@
 import AuthContext from 'context/AuthContext'
 import { deleteDoc, doc } from 'firebase/firestore'
-import { db } from 'firebaseApp'
+import { deleteObject, ref } from 'firebase/storage'
+import { db, storage } from 'firebaseApp'
 import { PostProps } from 'pages/home'
 import { useContext } from 'react'
 import { AiFillHeart } from 'react-icons/ai'
@@ -15,11 +16,19 @@ interface PostBoxProps {
 export default function PostBox({ post }: PostBoxProps) {
   const { user } = useContext(AuthContext)
   const navigate = useNavigate()
+  const imageRef = ref(storage, post?.imageUrl)
 
   const handleDelete = async () => {
     const confirm = window.confirm('해당 게시글을 삭제하시겠습니까?')
 
     if (confirm) {
+      // 이미지 삭제
+      if (post?.imageUrl) {
+        deleteObject(imageRef).catch((error: Error) => {
+          console.log(error)
+        })
+      }
+
       await deleteDoc(doc(db, 'posts', post.id))
       toast.success('게시글을 삭제했습니다.')
       navigate('/')
