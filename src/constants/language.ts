b53a810ffd53f1e75f842b2d74bfb0e7981910db
kabinny <kabinny@gmail.com>
2024-02-09 18:25:38 +0900

    useTranslation hook 생성

---
+++ src/constants/language.ts
@@ -0,0 +1,33 @@
+const TRANSLATIONS = {
+  // menu
+  MENU_HOME: {
+    ko: '홈',
+    en: 'Home',
+  },
+  MENU_PROFILE: {
+    ko: '프로필',
+    en: 'Profile',
+  },
+  MENU_SEARCH: {
+    ko: '검색',
+    en: 'Search',
+  },
+  MENU_NOTI: {
+    ko: '알림',
+    en: 'Notifications',
+  },
+  MENU_LOGOUT: {
+    ko: '로그아웃',
+    en: 'Logout',
+  },
+  MENU_LOGIN: {
+    ko: '로그인',
+    en: 'Login',
+  },
+  MENU_SIGNUP: {
+    ko: '회원가입',
+    en: 'Signup',
+  },
+}
+
+export default TRANSLATIONS
